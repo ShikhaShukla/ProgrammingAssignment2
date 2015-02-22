4f7ffd6b1f9ba4fa98dc15d6887b1648de2c0cc1
ShikhaShukla <shikhas@fractalanalytics.com>
2015-02-22 21:40:58 +0530

    cachematrixv1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## fun
+## Caching the Inverse of a Matrixctions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## Creates a list of functions that
+## can cache the inverse of a matrix.
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<-inverse
+    getInverse <- function() m
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {## Computes the inverse of the matrix returned
+## by makeCacheMatrix(), unless the inverse has
+## already been calculated, in which case
+## it retrieves it from the cache.
+    m <- x$getInverse()
+    if ( ! is.null(m)) {
+        print("getting cached data")
+        return(m)
+    }
+    m <- solve(x$get())
+    x$setInverse(m)
+    m
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+a <- makeCacheMatrix(matrix(1:4,2))
+a$get()
+a$getInverse()
+a$set(matrix(5:8,2))
+a$get()
+cacheSolve(a)
+cacheSolve(a)
+a$getInverse()
+b = a$getInverse()
+a$get() %*% b
